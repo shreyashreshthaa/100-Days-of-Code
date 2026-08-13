f195f8270f292e79d88e07617a7c5219705418c6
shreyashreshthaa <shreyashreshtha62@gmail.com>
2026-08-13 19:18:23 +0530

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,17 @@
+//Q1: Write a program to input two numbers and display their sum.//
+#include <stdio.h>
+int main()
+{
+int a;
+int b;
+int c;
+printf("Enter the value of a");
+scanf("%d", &a);
+printf("Enter the value of b");
+scanf("%d",&b);
+c=a+b;
+scanf("%d",&c);
+printf("The sum of %d and %d is %d", a,b,c);
+return 0;
+}
+
